d4cda21e7869254fc39c540a3b974b2b5dd61ec3
siralfbaez <siralf@urimagination.com>
2023-06-09 08:58:04 -0400

    Update dependencies: cloud.google.com/go, dmitri.shuralyov.com/gpu/mtl, github.com/B urntSushi/toml, github.com/BurntSushi/xgb, github.com/CloudyKit/fastprinter, github

--- middleware/requireAuth.go
+++ middleware/requireAuth.go
@@ -4,7 +4,7 @@ import "github.com/gofiber/fiber/v2"
 
 func RequireAuth(c *fiber.Ctx) error {
 	// Set a custom header on all responses:
-	c.Set("X-Custom-Header", "Hello, World")
+	c.Set("X-Custom-Header", "Hello, World!")
 
 	// Go to next middleware:
 	return c.Next()
